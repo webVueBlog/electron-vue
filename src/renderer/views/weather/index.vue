5d5dc981cee3872ee9f885f3ab816861e41b3181
webVueBlog <2979986590@qq.com>
2022-11-11 19:07:24 +0800

    create: electron-vue 实战

---
+++ src/renderer/views/weather/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-tag type="success">{{weather}}</el-tag>
+</template>
+
+<script>
+import request from 'request-promise'
+export default {
+  data() {
+    return {
+      weather: ''
+    }
+  },
+  mounted() {
+    const city = encodeURIComponent('深圳')
+    request({
+      // uri: `http://apis.juhe.cn/simpleWeather/query?city=${city}&key=bb888175aa5ed3bac588aa34d1eec123`,
+      uri: `http://www.tianqiapi.com/api?version=v9&appid=23035354&appsecret=8YvlPNrz&city=${city}`,
+      method: 'GET',
+      json: true
+    }).then(res => {
+      this.weather = `${res.city} ${res.data[0].week} ${res.data[0].tem}℃`
+    })
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
